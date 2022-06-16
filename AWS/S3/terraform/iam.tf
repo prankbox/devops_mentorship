6b34bd5d850bc971cafe0ad96826569d0d5e067e
Sergey <edelweissco@gmail.com>
2022-06-16 23:14:43 +0300

    Fixes policy

--- AWS/S3/terraform/iam.tf
+++ AWS/S3/terraform/iam.tf
@@ -4,7 +4,8 @@ data "aws_iam_policy" "s3" {
 
 resource "aws_iam_role" "s3full" {
   name               = "S3Full"
-  assume_role_policy = data.aws_iam_policy.s3.policy
+  assume_role_policy = data.aws_iam_policy.s3.name
+  
 }
 
 resource "aws_iam_instance_profile" "ec2_profile" {
