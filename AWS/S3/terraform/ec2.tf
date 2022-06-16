665aac51b3bcc3867ef3bb4d1d0685890a009fda
Sergey <edelweissco@gmail.com>
2022-06-16 17:33:02 +0300

    Fixes syntax in ec2.tf

--- AWS/S3/terraform/ec2.tf
+++ AWS/S3/terraform/ec2.tf
@@ -20,8 +20,8 @@ data "aws_ami" "amazon_linux" {
 resource "aws_instance" "control_plane" {
   ami                         = data.aws_ami.amazon_linux.id
   instance_type               = var.instance_type
-  subnet_id                   = aws_subnet.dev_subnet_1.id
-  vpc_security_group_ids      = [aws_security_group.dev_ec2_sg.id]
+  subnet_id                   = aws_subnet.public_subnet_a.id
+  vpc_security_group_ids      = [aws_security_group.public_a_sg]
   availability_zone           = data.aws_availability_zones.available.names[0]
   associate_public_ip_address = true
   key_name                    = aws_key_pair.ssh_key.key_name
