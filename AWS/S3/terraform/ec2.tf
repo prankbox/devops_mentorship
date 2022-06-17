c98f006d7803a80c97cbc0c0aa2e215e2100864d
Sergey <edelweissco@gmail.com>
2022-06-17 08:58:20 +0300

    Fixes script

--- AWS/S3/terraform/ec2.tf
+++ AWS/S3/terraform/ec2.tf
@@ -24,29 +24,29 @@ resource "aws_instance" "control_plane" {
   vpc_security_group_ids      = [aws_security_group.public_a_sg.id]
   availability_zone           = data.aws_availability_zones.available.names[0]
   associate_public_ip_address = true
-  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
+  iam_instance_profile        = aws_iam_instance_profile.ec2_profile.name
   key_name                    = aws_key_pair.ssh_key.key_name
-  user_data                   = <<EOF
-      #!/bin/bash
-      sudo amazon-linux-extras install epel -y
-      sudo yum update -y
-      sudo yum install s3fs-fuse nginx
-      sudo chown -R nginx /var/log/nginx
-      sudo s3fs ${local.s3_bucket_name} /var/log/nginx -o iam_role=S3Full -o allow_other -o use_cache=/tmp/cache
-      sudo systemct enable nginx --now
-    EOF
-
-
   tags = {
     Name = "${var.prefix}-Control-Plane"
   }
+  depends_on = [
+    aws_s3_bucket.bucket
+  ]
 }
 
 resource "null_resource" "check_connection" {
 
   provisioner "remote-exec" {
     inline = [
-      "echo \"Checking connection...\""
+      "echo \"Checking connection...\"",
+      "sudo amazon-linux-extras install epel -y",
+      "sudo yum update -y",
+      "sudo yum install s3fs-fuse nginx -y",
+      "sudo chown -R nginx /var/log/nginx",
+      "sudo echo \"s3fs#${local.s3_bucket_name} /var/log/nginx fuse _netdev,iam_role=S3Full,allow_other,umask=227,uid=995,gid=993,use_cache=/tmp/cache 0 0\" |sudo tee -a /etc/fstab",
+      #"sudo s3fs ${local.s3_bucket_name} /var/log/nginx -o iam_role=S3Full -o allow_other -o use_cache=/tmp/cache",
+      "sudo mount -a",
+      "sudo systemctl enable nginx --now"
     ]
     connection {
       type        = "ssh"
