addbc1b3cb9089fa52967765ebb0df358e180580
Sergey <edelweissco@gmail.com>
2022-06-22 07:02:09 +0300

    Deletes commented lines in lambda.tf

--- AWS/Lambda/TG/terraform/lambda.tf
+++ AWS/Lambda/TG/terraform/lambda.tf
@@ -8,14 +8,11 @@
 
 module "bot_lambda" {
   source = "terraform-aws-modules/lambda/aws"
-  #version = "2.7.0"
-  # insert the 28 required variables here
   function_name = "telegram-cripto-bot"
   description   = "Gets btc rate"
   handler       = "handler.bot"
   runtime       = "python3.9"
   source_path   = "${path.module}/bot"
-  #environment_variables = [var.token]
   environment_variables = {
     TELEGRAM_TOKEN = var.token
   }
