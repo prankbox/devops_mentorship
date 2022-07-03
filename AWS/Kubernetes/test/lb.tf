56eb84821e4f49a5f0c7a867b61f2e9c18f5b60b
Sergey <edelweissco@gmail.com>
2022-07-03 21:02:44 +0300

    Commented out lb

--- AWS/Kubernetes/test/lb.tf
+++ AWS/Kubernetes/test/lb.tf
@@ -1,63 +1,67 @@
-resource "aws_lb" "this" {
-  name               = "basic-load-balancer"
-  load_balancer_type = "network"
-  subnets            = [aws_subnet.public_a.id]
+# resource "aws_lb" "this" {
+#   name               = "basic-load-balancer"
+#   load_balancer_type = "network"
+#   subnets            = [aws_subnet.public_a.id]
 
-  enable_cross_zone_load_balancing = true
-}
+#   enable_cross_zone_load_balancing = true
+# }
 
 
-resource "aws_lb_listener" "this" {
-  #for_each = var.ports
+# resource "aws_lb_listener" "this" {
+#   #for_each = var.ports
 
-  load_balancer_arn = aws_lb.this.arn
+#   load_balancer_arn = aws_lb.this.arn
 
-  protocol = "TCP"
-  port     = "6443"
+#   protocol = "TCP"
+#   port     = "6443"
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.this.arn
-  }
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.this.arn
+#   }
 
-}
+# }
 
-resource "aws_lb_target_group" "this" {
-  #for_each = var.ports
+# resource "aws_lb_target_group" "this" {
+#   #for_each = var.ports
 
-  port     = "6443"
-  protocol = "TCP"
-  vpc_id   = aws_vpc.main.id
+#   port     = "6443"
+#   protocol = "TCP"
+#   vpc_id   = aws_vpc.main.id
 
-  //stickiness = []
+#   //stickiness = []
 
-  depends_on = [
-    aws_lb.this
-  ]
+#   depends_on = [
+#     aws_lb.this
+#   ]
 
-  lifecycle {
-    create_before_destroy = true
-  }
-}
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
 
-data "aws_instances" "nlb_insts" {
-  instance_state_names = ["running"]
+# data "aws_instances" "nlb_insts" {
+#   instance_state_names = ["running"]
 
-  depends_on = [
-    module.ec2_instance
-  ]
-}
+#   depends_on = [
+#     module.ec2_instance
+#   ]
+# }
 
-resource "aws_lb_target_group_attachment" "this" {
-  target_group_arn = aws_lb_target_group.this.arn
-  target_id        = [for sc in range(var.inst_count) : module.ec2_instance.id[sc]]
-  port             = 6443
+# resource "aws_lb_target_group_attachment" "this" {
+#   target_group_arn = aws_lb_target_group.this.arn
+#   target_id        = [for sc in range(var.inst_count) : module.ec2_instance.id[sc]]
+#   port             = 6443
 
-  depends_on = [
-    module.ec2_instance,
-    aws_lb_target_group.this
-  ]
-}
+#   depends_on = [
+#     module.ec2_instance,
+#     aws_lb_target_group.this
+#   ]
+# }
+
+
+
+
 
 # resource "aws_autoscaling_attachment" "target" {
 #   for_each = var.ports
