efe18362b0a049c8ed6a46f00be7a7a583632243
Sergey <edelweissco@gmail.com>
2022-07-04 18:51:30 +0300

    Removes some from  init scripts

--- AWS/Kubernetes/Infra/scripts/worker_init.sh
+++ AWS/Kubernetes/Infra/scripts/worker_init.sh
@@ -1,5 +1,5 @@
 sudo apt update -y
-sudo apt full-upgrade -y --auto-remove
+#sudo apt full-upgrade -y --auto-remove
 sudo apt remove multipath-tools -y
 sudo hostnameclt set-hostname worker-"${count.index}"
-sudo reboot now
+#sudo reboot now
