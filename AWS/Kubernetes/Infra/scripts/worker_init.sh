f64ae55ae2f329536befb99931c0125c6f504dc1
Sergey <edelweissco@gmail.com>
2022-07-04 18:45:49 +0300

    Adds params to init scripts

--- AWS/Kubernetes/Infra/scripts/worker_init.sh
+++ AWS/Kubernetes/Infra/scripts/worker_init.sh
@@ -1,5 +1,5 @@
 sudo apt update -y
-sudo apt upgrade -y
+sudo apt full-upgrade -y --auto-remove
 sudo apt remove multipath-tools -y
 sudo hostnameclt set-hostname worker-"${count.index}"
-sudo reboot
+sudo reboot now
